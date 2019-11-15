edf65149ac69370f46bfd08b3e9527183248451d
Won <trilliwon@gmail.com>
2019-11-15 15:09:30 +0900

    Implement SignIn

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { connect } from 'react-redux'
+import LoadingBar from 'react-redux-loading'
 import { handleInitialData } from '../actions/shared'
+import Home from './Home'
+import Nav from './Nav'
+import SignIn from './SignIn'
 
 class App extends Component {
 
@@ -10,12 +15,29 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        Starter Code
-      </div>
+      <Router>
+        <Fragment>
+          <LoadingBar />
+          <Router>
+            <div className='container'>
+              {this.props.authedUser === null
+                ? <SignIn />
+                : <div>
+                  <Nav />
+                  <Route path='/' exact component={Home} />
+                </div>}
+            </div>
+          </Router>
+        </Fragment>
+      </Router>
     );
   }
 }
 
+function mapStateToProps({ authedUser }) {
+  return {
+    authedUser,
+  }
+}
 
-export default connect()(App);
+export default connect(mapStateToProps)(App);
