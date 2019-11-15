c338ec94583738dd42b40a48783178794f507127
Won <trilliwon@gmail.com>
2019-11-15 15:07:31 +0900

    Implement Nav, Question, SignIn Components

---
+++ src/components/SignIn.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import { Form } from 'react-bootstrap'
+import { connect } from 'react-redux'
+import { setAuthedUser } from '../actions/authedUser'
+
+class SignIn extends Component {
+
+    handleUserSelection = (e) => {
+        const id = e.target.value
+        const { dispatch } = this.props
+        dispatch(setAuthedUser(id))
+    }
+
+    render() {
+        const { users } = this.props
+        return (
+            <div className='container'>
+                <div>
+                    <h2>Welcome to the Would You Rather App</h2>
+                    <h4>Pease sign in to continue</h4>
+                </div>
+                <div className='container'>
+                    <Form>
+                        <Form.Group controlId="formGridState">
+                            <Form.Label>Sign In</Form.Label>
+                            <Form.Control as="select" onChange={this.handleUserSelection}>
+                                <option>Select User</option>
+                                {users.map((user) => (
+                                    <option key={user.id} value={user.id}>{user.name}</option>
+                                ))}
+                            </Form.Control>
+                        </Form.Group>
+                    </Form>
+                </div>
+            </div>
+        )
+    }
+}
+
+function mapStateToProps({ users }) {
+    return {
+        users: Object.keys(users).map((id) => users[id])
+    }
+}
+
+export default connect(mapStateToProps)(SignIn)
