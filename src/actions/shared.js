73dbcf76d8078b422c2fddbfa04de67f28bca9e7
Won <trilliwon@gmail.com>
2019-11-18 18:23:04 +0900

    Change AUTHED_ID to null as initial state

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -4,7 +4,7 @@ import { receiveQuestions } from '../actions/questions'
 import { setAuthedUser } from '../actions/authedUser'
 import { showLoading, hideLoading } from 'react-redux-loading'
 
-let AUTHED_ID = null
+let AUTHED_ID = null 
 
 export function handleInitialData() {
     return (dispatch) => {
