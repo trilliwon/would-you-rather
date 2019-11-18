a98a6def9ab6e76743064ade108cfc174238d301
Won <trilliwon@gmail.com>
2019-11-18 18:22:26 +0900

    Add actions for users

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,8 +1,27 @@
 export const RECEIVE_USERS = 'RECEIVE_USERS'
+export const ADD_ANSWER_TO_USER = 'ADD_ANSWER_TO_USER'
+export const ADD_NEW_QUESTION_TO_USER = 'ADD_NEW_QUESTION_TO_USER' 
 
 export function receiveUsers(users) {
     return {
         type: RECEIVE_USERS,
         users,
     }   
+}
+
+export function addAnswerToUser(authedUser, qid, answer) {
+    return {
+        type: ADD_ANSWER_TO_USER,
+        authedUser,
+        qid,
+        answer
+    }
+}
+
+export function addNewQuestionToUser(author, qid) {
+    return {
+        type: ADD_NEW_QUESTION_TO_USER,
+        author,
+        qid,
+    }    
 }
