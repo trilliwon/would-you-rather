8d13fbbfb2a18d34f2a34509319e778d4f31cbbf
Won <trilliwon@gmail.com>
2019-11-18 18:24:49 +0900

    Add reducers for users

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,4 +1,8 @@
-import { RECEIVE_USERS } from '../actions/users'
+import {
+    RECEIVE_USERS,
+    ADD_ANSWER_TO_USER,
+    ADD_NEW_QUESTION_TO_USER
+} from '../actions/users'
 
 export default function users(state = {}, action) {
     switch (action.type) {
@@ -6,6 +10,26 @@ export default function users(state = {}, action) {
             return {
                 ...state,
                 ...action.users,
+            }
+        case ADD_ANSWER_TO_USER:
+            const { authedUser, qid, answer } = action
+            return {
+                ...state,
+                [authedUser]: {
+                    ...state[authedUser],
+                    answers: {
+                        ...state[authedUser].answers,
+                        [qid]: answer,
+                    }
+                }
+            }
+        case ADD_NEW_QUESTION_TO_USER:
+            return {
+                ...state,
+                [action.author] : {
+                    ...state[action.author],
+                    questions: state[action.author].questions.concat([action.qid])
+                }
             }
         default:
             return state
