98a1b572972330cf4ec2a06cb7a7d0d7453cb347
Won <trilliwon@gmail.com>
2019-11-18 18:25:05 +0900

    Add reducers for questions

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,4 +1,8 @@
-import { RECEIVE_QUESTIONS } from '../actions/questions'
+import {
+    RECEIVE_QUESTIONS,
+    ADD_ANSWER_TO_QUESTION,
+    SAVE_NEW_QUESTION
+} from '../actions/questions'
 
 export default function questions(state = {}, action) {
     switch (action.type) {
@@ -6,6 +10,23 @@ export default function questions(state = {}, action) {
             return {
                 ...state,
                 ...action.questions,
+            }
+        case SAVE_NEW_QUESTION:
+            return {
+                ...state,
+                [action.question.id]: action.question
+            }
+        case ADD_ANSWER_TO_QUESTION:
+            const { authedUser, qid, answer } = action
+            return {
+                ...state,
+                [qid]: {
+                    ...state[qid],
+                    [answer]: {
+                        ...state[qid][answer],
+                        votes: state[qid][answer].votes.concat([authedUser]),
+                    }
+                }
             }
         default:
             return state
